3ae54249ffe6eaeedfbcfbb955bc1c6821e6c9c7
stephanie-ai <stephanie_cooper@hotmail.com>
2020-12-03 17:45:18 +0000

    create onChange and onSubmit functions

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,20 +3,40 @@ import React, { Component } from 'react'
 class PostForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            title: '',
+            body: ''
+        }
+        // this.onChange = this.onChange.bind(this)
     }
+
+    onChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+        // fetch(https://jsonplaceholder.typicode.com/posts)
+    }
+
     render() {
         return (
             <div>
                 <h1>Add Post</h1>
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div>
                         <label>Title: </label><br />
-                        <input type="text" name="title" />
+                        <input type="text" name="title" onChange={this.onChange} value={this.state.title} />
                     </div>
                     <div>
                         <label>Body: </label><br />
-                        <textarea name="body" />
+                        <textarea name="body" onChange={this.onChange} value={this.state.body} />
                     </div>
                     <br />
                     <button type="submit">Submit</button>
