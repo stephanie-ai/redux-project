0259c1ca95600cd7cc605e841a7d61f2085958e2
stephanie-ai <stephanie_cooper@hotmail.com>
2020-12-03 17:38:04 +0000

    add form with an input and submit button

---
+++ src/components/PostForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class PostForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>
+                <h1>Add Post</h1>
+                <form>
+                    <div>
+                        <label>Title: </label><br />
+                        <input type="text" name="title" />
+                    </div>
+                    <div>
+                        <label>Body: </label><br />
+                        <textarea name="body" />
+                    </div>
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default PostForm;
