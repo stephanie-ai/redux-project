6b91ded539085cf01863667255903f96180df25a
stephanie-ai <stephanie_cooper@hotmail.com>
2020-12-04 22:22:19 +0000

    add mSTP and use this.props

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions;
+
+// connect will connect the components to the redux store that is provided by the provider component
 
 class Posts extends Component {
 
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
-        const postItems = this.state.posts.map(post => (
+        const postItems = this.props.posts.map(post => (
             <div key={post.id}>
                 <h3>{post.title}</h3>
                 <p>{post.body}</p>
@@ -18,4 +26,13 @@ class Posts extends Component {
     }
 }
 
-export default Posts;
+// we have to get the new items from the state using mSTP and then map the properties to the component and use that inside
+
+const mSTP = state => ({
+    posts: state.posts.items
+});
+
+export default connect(mSTP, { fetchPosts })(Posts);
+
+// this is where we want to map our state to our properties and we are not doing that yet so put null
+// second parameter is the fetchPosts function which just calls the fetch API request
