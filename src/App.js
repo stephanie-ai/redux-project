abc1c976d9e758d820b5753490f1cf2f665ce0d0
stephanie-ai <stephanie_cooper@hotmail.com>
2020-12-04 21:58:49 +0000

    use provider and store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { Provider } from 'react-redux';
+
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
+
+import store from './store';
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <>
         <h1>Welcome</h1>
         <PostForm />
         <Posts />
       </>
+      </Provider>
     )
   }
 }
